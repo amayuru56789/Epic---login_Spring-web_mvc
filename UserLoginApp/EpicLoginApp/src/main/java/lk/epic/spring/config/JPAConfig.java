b6fb29a7376566e3fd9cf0db4dc9d81ab6770d85
amayuru56789 <indeewara5678@gmail.com>
2022-11-29 23:06:56 +0530

    added parent context & childContext

---
+++ UserLoginApp/EpicLoginApp/src/main/java/lk/epic/spring/config/JPAConfig.java
@@ -0,0 +1,4 @@
+package lk.epic.spring.config;
+
+public class JPAConfig {
+}
---
+++ UserLoginApp/EpicLoginApp/src/main/java/lk/epic/spring/config/WebAppConfig.java
@@ -0,0 +1,12 @@
+package lk.epic.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "lk.epic.spring")
+public class WebAppConfig {
+
+}
