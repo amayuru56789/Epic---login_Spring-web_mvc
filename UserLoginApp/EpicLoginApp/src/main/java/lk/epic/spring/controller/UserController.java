f166d185c856d99c10070521f9a94a87529cf540
amayuru56789 <indeewara5678@gmail.com>
2022-12-02 10:20:08 +0530

    complete back-end process & CRUD done for EpicLoginApp

--- UserLoginApp/EpicLoginApp/src/main/java/lk/epic/spring/controller/UserController.java
+++ UserLoginApp/EpicLoginApp/src/main/java/lk/epic/spring/controller/UserController.java
@@ -1,10 +1,11 @@
 package lk.epic.spring.controller;
 
+import lk.epic.spring.DTO.UserDTO;
+import lk.epic.spring.services.UserService;
+import lk.epic.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Amayuru indeewara
@@ -17,8 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public void getAllUser(){
-        System.out.println("hello");
+    public ResponseUtil getAllUser(){
+
+        return new ResponseUtil(200, "Ok", userService.getAllUsers());
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveUser(@ModelAttribute UserDTO user){
+        userService.saveUser(user);
+        return new ResponseUtil(200, "Ok", null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateUser(@RequestBody UserDTO user){
+        userService.updateUser(user);
+        return new ResponseUtil(200, "Ok", null);
+    }
+
+    @DeleteMapping(params = {"userID"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteUser(@RequestParam String userID){
+        userService.deleteUser(userID);
+        return new ResponseUtil(200, "Ok", null);
+    }
+
+    @GetMapping(path = "/{userID}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchUser(@PathVariable String userID){
+        return new ResponseUtil(200, "Ok", userService.searchUser(userID));
     }
 }
--- UserLoginApp/EpicLoginApp/src/main/java/lk/epic/spring/services/Impl/UserServiceImpl.java
+++ UserLoginApp/EpicLoginApp/src/main/java/lk/epic/spring/services/Impl/UserServiceImpl.java
@@ -1,76 +1,76 @@
-//package lk.epic.spring.services.Impl;
-//
-//import lk.epic.spring.DTO.UserDTO;
-//import lk.epic.spring.Entity.User;
-//import lk.epic.spring.repo.UserRepo;
-//import lk.epic.spring.services.UserService;
-//import org.modelmapper.ModelMapper;
-//import org.modelmapper.TypeToken;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//
-///**
-// * @author Amayuru indeewara
-// * @created 30/11/2022 - 19:08
-// * @project EpicLoginApp
-// */
-//@Service
-//@Transactional
-//public class UserServiceImpl implements UserService {
-//
-//    @Autowired
-//    private UserRepo userRepo;
-//
-//    @Autowired
-//    private ModelMapper modelMapper;
-//
-//    @Override
-//    public void saveUser(UserDTO dto) {
-//        if (!userRepo.existsById(dto.getUserID())){
-//            User entity = modelMapper.map(dto, User.class);
-//            userRepo.save(entity);
-//        }else {
-//            throw new RuntimeException("User already exists...");
-//        }
-//    }
-//
-//    @Override
-//    public void deleteUser(String userID) {
-//        if (userRepo.existsById(userID)){
-//            userRepo.deleteById(userID);
-//        }else {
-//            throw new RuntimeException("Please check the userID.. no such as user...!");
-//        }
-//    }
-//
-//    @Override
-//    public void updateUser(UserDTO dto) {
-//        if (userRepo.existsById(dto.getUserID())){
-//            User entity = modelMapper.map(dto, User.class);
-//            userRepo.save(entity);
-//        }else {
-//            throw new RuntimeException("no such as user to update.. please Check the userID...!");
-//        }
-//    }
-//
-//    @Override
-//    public UserDTO searchUser(String userID) {
-//        if (userRepo.existsById(userID)){
-//            User user = userRepo.findById(userID).get();
-//            return modelMapper.map(user, UserDTO.class);
-//        }else {
-//            throw new RuntimeException("no user for "+userID+"...!");
-//        }
-//    }
-//
-//    @Override
-//    public List<UserDTO> getAllUsers() {
-//        List<User> all = userRepo.findAll();
-//        return modelMapper.map(all, new TypeToken<List<UserDTO>>(){
-//
-//        }.getType());
-//    }
-//}
+package lk.epic.spring.services.Impl;
+
+import lk.epic.spring.DTO.UserDTO;
+import lk.epic.spring.Entity.User;
+import lk.epic.spring.repo.UserRepo;
+import lk.epic.spring.services.UserService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * @author Amayuru indeewara
+ * @created 30/11/2022 - 19:08
+ * @project EpicLoginApp
+ */
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public void saveUser(UserDTO dto) {
+        if (!userRepo.existsById(dto.getUserID())){
+            User entity = modelMapper.map(dto, User.class);
+            userRepo.save(entity);
+        }else {
+            throw new RuntimeException("User already exists...");
+        }
+    }
+
+    @Override
+    public void deleteUser(String userID) {
+        if (userRepo.existsById(userID)){
+            userRepo.deleteById(userID);
+        }else {
+            throw new RuntimeException("Please check the userID.. no such as user...!");
+        }
+    }
+
+    @Override
+    public void updateUser(UserDTO dto) {
+        if (userRepo.existsById(dto.getUserID())){
+            User entity = modelMapper.map(dto, User.class);
+            userRepo.save(entity);
+        }else {
+            throw new RuntimeException("no such as user to update.. please Check the userID...!");
+        }
+    }
+
+    @Override
+    public UserDTO searchUser(String userID) {
+        if (userRepo.existsById(userID)){
+            User user = userRepo.findById(userID).get();
+            return modelMapper.map(user, UserDTO.class);
+        }else {
+            throw new RuntimeException("no user for "+userID+"...!");
+        }
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<User> all = userRepo.findAll();
+        return modelMapper.map(all, new TypeToken<List<UserDTO>>(){
+
+        }.getType());
+    }
+}
